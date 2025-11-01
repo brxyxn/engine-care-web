fa20987fd7c1c9a84d2aff7c0df5cea0c35d2b3a
Brayan Lopez <brxyxn.corp@live.com>
2025-11-01 14:20:41 -0600

    chore: fix eslint config and versioning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,57 +6,57 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    {
-        ignores: [
-            "node_modules/**",
-            ".next/**",
-            "out/**",
-            "build/**",
-            "next-env.d.ts",
-            "eslint.config.mjs",
-            "postcss.config.mjs",
-            "tailwind.config.mjs",
-            "jest.config.mjs",
-            "prettier.config.cjs",
-            "src/redux/storage.ts",
-        ],
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "eslint.config.mjs",
+      "postcss.config.mjs",
+      "tailwind.config.mjs",
+      "jest.config.mjs",
+      "prettier.config.cjs",
+      "src/redux/storage.ts",
+    ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname,
+      },
     },
-    ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
-    {
-        languageOptions: {
-            parserOptions: {
-                project: ["./tsconfig.json"],
-                tsconfigRootDir: __dirname,
-            },
+    rules: {
+      // your custom rules here
+      "no-console": "warn",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      // "no-unused-vars": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-unused-vars": "off",
+      // enable TS-aware version with underscore escape hatch
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^(_|ignore)$",
+          destructuredArrayIgnorePattern: "^_",
         },
-        rules: {
-            // your custom rules here
-            "no-console": "warn",
-            "@typescript-eslint/no-unsafe-argument": "off",
-            // "no-unused-vars": "error",
-            "no-var": "error",
-            "prefer-const": "error",
-            "no-unused-vars": "off",
-            // enable TS-aware version with underscore escape hatch
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                    vars: "all",
-                    args: "after-used",
-                    ignoreRestSiblings: true,
-                    varsIgnorePattern: "^_",
-                    argsIgnorePattern: "^_",
-                    caughtErrors: "all",
-                    caughtErrorsIgnorePattern: "^(_|ignore)$",
-                    destructuredArrayIgnorePattern: "^_",
-                },
-            ],
-        },
+      ],
     },
+  },
 ];
 
 export default eslintConfig;
