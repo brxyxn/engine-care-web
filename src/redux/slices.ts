ac1330e638d1ddeca324ff11e807fc229ef83a59
Brayan Lopez <brxyxn.corp@live.com>
2025-11-02 12:43:39 -0600

    feat: update slices import in store for a cleaner approach

---
+++ src/redux/slices.ts
@@ -0,0 +1,6 @@
+import { counterSlice } from "@/redux/counter/counter-slice"
+import { customerSlice } from "@/redux/customers/customer-slice"
+
+const allSlices = [counterSlice, customerSlice]
+
+export default allSlices
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,38 +1,15 @@
-// `combineSlices` automatically combines the reducers using
-// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  persistReducer,
-  persistStore,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from "redux-persist"
-import { counterSlice } from "@/redux/counter/counter-slice"
-import { customerSlice } from "@/redux/customers/customer-slice"
+import { Action, combineSlices, configureStore, ThunkAction, } from "@reduxjs/toolkit"
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE, } from "redux-persist"
 import { logger } from "@/redux/middleware/logger"
+import allSlices from "@/redux/slices"
 import storage from "@/redux/storage"
-import {
-  Action,
-  combineSlices,
-  configureStore,
-  ThunkAction,
-} from "@reduxjs/toolkit"
 
 const persistConfig = {
   key: "root",
   storage,
 }
 
-// `combineSlices` automatically combines the reducers using
-// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-// const rootReducer = combineReducers({
-//   [counterSlice.reducerPath]: counterSlice.reducer,
-//   [customerSlice.reducerPath]: customerSlice.reducer,
-// })
-export const rootReducer = combineSlices(counterSlice, customerSlice)
+export const rootReducer = combineSlices(...allSlices)
 export type RootState = ReturnType<typeof rootReducer>
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
@@ -49,17 +26,11 @@ export const store = configureStore({
 
 export const persistor = persistStore(store)
 
-// `makeStore` encapsulates the store configuration to allow
-// creating unique store instances, which is particularly important for
-// server-side rendering (SSR) scenarios. In SSR, separate store instances
-// are needed for each request to prevent cross-request state pollution.
 export const makeStore = () => {
   return store
 }
 
-// Infer the return type of `makeStore`
 export type AppStore = ReturnType<typeof makeStore>
-// Infer the `AppDispatch` type from the store itself
 export type AppDispatch = AppStore["dispatch"]
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
